265cd413156b4a485081816e347df44c92884491
Joan Bascur <naojimagot@gmail.com>
2012-05-04 00:22:54 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaginaRol::Application.config.secret_token = '3b6897cfbafaa70508246219e1971f3772b61af563747e0abbcd0c8984356987d30eb90ab5122c27698b49102b3a5e0c5dd1ec7c309845352cb5947a8f5c478d'
