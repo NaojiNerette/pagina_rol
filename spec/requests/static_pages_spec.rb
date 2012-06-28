7dd0261fedc168a67e628ff1e5bef073b30e9c56
Joan Bascur <naojimagot@gmail.com>
2012-06-27 23:36:25 -0300

    Modificacion nuevo estilo - header

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   describe "Pagina de inicio" do
     before { visit root_path }
 
-    it { should have_selector('h1', text: 'Kairn Telest') }
+    #it { should have_selector('h1', text: 'Kairn Telest') }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector('title', text: '| Inicio') }
   end
@@ -22,7 +22,7 @@ describe "Static pages" do
   describe "Pagina de acerca" do
     before { visit about_path }
     
-    it { should have_selector('h1', text: 'About') }
+    it { should have_selector('h1', text: 'Acerca de') }
     it { should have_selector('title', text: full_title('About')) }
   end
 
