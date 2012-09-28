088f506312a97e29a3280cb44593c184dc76eadc
Joan Bascur <naojimagot@gmail.com>
2012-09-28 01:02:38 -0300

    Actualizacion del titulo del sitio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Las puertas de Kairn Telest"
+    base_title = "Las puertas de Kairn Re'ox"
     if page_title.empty?
       base_title
     else
--- app/helpers/application_helper.rb~
+++ app/helpers/application_helper.rb~
@@ -9,5 +9,10 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  def markdown(text)  
+    Redcarpet.new(text).to_html.html_safe  
+  end  
+  
 end
 
